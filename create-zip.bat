0168ecd8ff2dc51fbc3758fa95341a2bfebf8577
Ziplaw <afrgplus@gmail.com>
2025-07-07 13:20:57 +0200

    bundled pillow

---
+++ create-zip.bat
@@ -0,0 +1,1 @@
+tar -a -c -f output.zip *
--- imageio_test.py
+++ imageio_test.py
@@ -1,9 +1,9 @@
 bl_info = {
-    "name": "Batch Exporter",
+    "name": "UV Plotter",
     "author": "Ziplaw",
     "version": (1, 0),
     "blender": (3, 6, 14),
-    "description": "Exports in FBX in a given directory",
+    "description": "UV Plotter Description",
 }
 
 import sys
@@ -11,137 +11,137 @@ import imageio.v3 as iio
 import math
 from mathutils import Vector
 
-import bpy
-import os
+# import bpy
+# import os
 
-class BatchExportPropertyGroup(bpy.types.PropertyGroup):
-    filepath : bpy.props.StringProperty(
-        name="Path",
-        description="Path to export all models",
-        default="C:/tmp/",
-        )
-    only_selected : bpy.props.BoolProperty(
-        name="Only Selected",
-        description="Only export selected models",
-        default=True,
-        )
-    apply_rotations : bpy.props.BoolProperty(
-        name="Apply Rotation and Scale",
-        description="Apply Rotation and Scale before exporting",
-        default=False,
-        )
-    move_to_world_center : bpy.props.BoolProperty(
-        name="Reset Position",
-        description="Moves each object to 0,0,0 before exporting and back to their original position afterwards",
-        default=True,
-        )
-    export_settings : bpy.props.EnumProperty(
-        items=
-        [
-            ('UNITY', "Unity", "Unity's Export Settings",1),
-            ('UE5', "Unreal Engine 5", "Unreal Engine 5's Export Settings",2),
-        ],
-        name="Export Settings",
-        description="",
-        default='UNITY'
-        )
+# class BatchExportPropertyGroup(bpy.types.PropertyGroup):
+#     filepath : bpy.props.StringProperty(
+#         name="Path",
+#         description="Path to export all models",
+#         default="C:/tmp/",
+#         )
+#     only_selected : bpy.props.BoolProperty(
+#         name="Only Selected",
+#         description="Only export selected models",
+#         default=True,
+#         )
+#     apply_rotations : bpy.props.BoolProperty(
+#         name="Apply Rotation and Scale",
+#         description="Apply Rotation and Scale before exporting",
+#         default=False,
+#         )
+#     move_to_world_center : bpy.props.BoolProperty(
+#         name="Reset Position",
+#         description="Moves each object to 0,0,0 before exporting and back to their original position afterwards",
+#         default=True,
+#         )
+#     export_settings : bpy.props.EnumProperty(
+#         items=
+#         [
+#             ('UNITY', "Unity", "Unity's Export Settings",1),
+#             ('UE5', "Unreal Engine 5", "Unreal Engine 5's Export Settings",2),
+#         ],
+#         name="Export Settings",
+#         description="",
+#         default='UNITY'
+#         )
         
         
-    def draw(self,context):
-        self.layout.prop(self,"export_settings")
+#     def draw(self,context):
+#         self.layout.prop(self,"export_settings")
 
-class BatchExportOperator(bpy.types.Operator):
-    """Tooltip"""
-    bl_idname = "object.batch_export_operator"
-    bl_label = "Batch Export"
+# class BatchExportOperator(bpy.types.Operator):
+#     """Tooltip"""
+#     bl_idname = "object.batch_export_operator"
+#     bl_label = "Batch Export"
 
-    @classmethod
-    def poll(cls, context):
-        return True
+#     @classmethod
+#     def poll(cls, context):
+#         return True
 
-    def execute(self, context):
-        props = context.scene.BatchExportPropertyGroup
+#     def execute(self, context):
+#         props = context.scene.BatchExportPropertyGroup
         
-        if props.only_selected:
-            selected_objects = context.selected_objects
-        else:
-            selected_objects = bpy.data.objects
+#         if props.only_selected:
+#             selected_objects = context.selected_objects
+#         else:
+#             selected_objects = bpy.data.objects
         
         
-        for object in selected_objects:
-            object.select_set(False)
+#         for object in selected_objects:
+#             object.select_set(False)
         
-        for object in selected_objects:
-            object.select_set(True)
+#         for object in selected_objects:
+#             object.select_set(True)
             
-            if props.apply_rotations:
-                bpy.ops.object.transform_apply(location=False, rotation=True, scale=True)
+#             if props.apply_rotations:
+#                 bpy.ops.object.transform_apply(location=False, rotation=True, scale=True)
                 
-            original_position = object.location
+#             original_position = object.location
             
-            if props.move_to_world_center:
-                object.location = 0,0,0
-                print(original_position)
+#             if props.move_to_world_center:
+#                 object.location = 0,0,0
+#                 print(original_position)
                             
-            match props.export_settings:
-                case 'UNITY':
-                    bpy.ops.export_scene.fbx(filepath= (f"{props.filepath}/{object.name}.fbx"),use_selection=True,apply_scale_options='FBX_SCALE_ALL',object_types={'MESH'},bake_space_transform=True, apply_unit_scale=True, add_leaf_bones=False, use_armature_deform_only=False, bake_anim=False,axis_forward='-Z', axis_up = 'Y')
+#             match props.export_settings:
+#                 case 'UNITY':
+#                     bpy.ops.export_scene.fbx(filepath= (f"{props.filepath}/{object.name}.fbx"),use_selection=True,apply_scale_options='FBX_SCALE_ALL',object_types={'MESH'},bake_space_transform=True, apply_unit_scale=True, add_leaf_bones=False, use_armature_deform_only=False, bake_anim=False,axis_forward='-Z', axis_up = 'Y')
                     
-                case 'UE5':
-                    bpy.ops.export_scene.fbx(filepath= (f"{props.filepath}/{object.name}.fbx"),use_selection=True,apply_scale_options='FBX_SCALE_ALL',object_types={'MESH'},bake_space_transform=True, apply_unit_scale=True, add_leaf_bones=False, use_armature_deform_only=False, bake_anim=False,axis_forward='-Z', axis_up = 'Y')
+#                 case 'UE5':
+#                     bpy.ops.export_scene.fbx(filepath= (f"{props.filepath}/{object.name}.fbx"),use_selection=True,apply_scale_options='FBX_SCALE_ALL',object_types={'MESH'},bake_space_transform=True, apply_unit_scale=True, add_leaf_bones=False, use_armature_deform_only=False, bake_anim=False,axis_forward='-Z', axis_up = 'Y')
                     
-                case _:
-                    raise Exception('Export Settings not Implemented')
+#                 case _:
+#                     raise Exception('Export Settings not Implemented')
             
-            if props.move_to_world_center:
-                print(original_position)
-                object.location = original_position
+#             if props.move_to_world_center:
+#                 print(original_position)
+#                 object.location = original_position
         
-        os.startfile(props.filepath)
-        return {'FINISHED'}
+#         os.startfile(props.filepath)
+#         return {'FINISHED'}
 
-class BatchExporter(bpy.types.Panel):
-    """Creates a Panel for exporting objects in batch."""
-    bl_label = "Batch Exporter"
-    bl_idname = "PT_BatchExporter"
-    bl_space_type = 'VIEW_3D'
-    bl_region_type = 'UI'
-    bl_category = 'Batch Export'
+# class BatchExporter(bpy.types.Panel):
+#     """Creates a Panel for exporting objects in batch."""
+#     bl_label = "Batch Exporter"
+#     bl_idname = "PT_BatchExporter"
+#     bl_space_type = 'VIEW_3D'
+#     bl_region_type = 'UI'
+#     bl_category = 'Batch Export'
 
-    def draw(self, context):
-        layout = self.layout
+#     def draw(self, context):
+#         layout = self.layout
 
-        props = context.scene.BatchExportPropertyGroup
+#         props = context.scene.BatchExportPropertyGroup
 
-        layout.prop(props, "filepath")
-        layout.prop(props, "only_selected")
-        layout.prop(props, "apply_rotations")
-        layout.prop(props, "move_to_world_center")
-        layout.prop(props, "export_settings")
+#         layout.prop(props, "filepath")
+#         layout.prop(props, "only_selected")
+#         layout.prop(props, "apply_rotations")
+#         layout.prop(props, "move_to_world_center")
+#         layout.prop(props, "export_settings")
         
-        row = layout.row()
-        op = row.operator("object.batch_export_operator")
+#         row = layout.row()
+#         op = row.operator("object.batch_export_operator")
 
 
-def register():
-    bpy.utils.register_class(BatchExporter)
-    bpy.utils.register_class(BatchExportOperator)
-    bpy.utils.register_class(BatchExportPropertyGroup)
-    bpy.types.Scene.BatchExportPropertyGroup = bpy.props.PointerProperty(
-            type=BatchExportPropertyGroup)
+# def register():
+#     bpy.utils.register_class(BatchExporter)
+#     bpy.utils.register_class(BatchExportOperator)
+#     bpy.utils.register_class(BatchExportPropertyGroup)
+#     bpy.types.Scene.BatchExportPropertyGroup = bpy.props.PointerProperty(
+#             type=BatchExportPropertyGroup)
 
 
-def unregister():
-    bpy.utils.unregister_class(BatchExporter)
-    bpy.utils.unregister_class(BatchExportOperator)
-    bpy.utils.unregister_class(BatchExportPropertyGroup)
-    del bpy.types.Scene.MyPropertyGroup
+# def unregister():
+#     bpy.utils.unregister_class(BatchExporter)
+#     bpy.utils.unregister_class(BatchExportOperator)
+#     bpy.utils.unregister_class(BatchExportPropertyGroup)
+#     del bpy.types.Scene.MyPropertyGroup
 
 
-if __name__ == "__main__":
-    register()
+# if __name__ == "__main__":
+#     register()
 
-image = iio.imread(r"C:\Users\Usuario\src\repos\bl-uv-plotter-testimages\T_Main_Color_Palette.png")
+image = iio.imread(r"C:\Users\Usuario\source\repos\bl-uv-plotter-testimages\T_Main_Color_Palette.png")
 
 width = image.shape[1]
 height = image.shape[0]
